2aa38ee592571ffa85b06032d5ca0ef58e5c9e4c
crazyfreeman77 <104940633+crazyfreeman77@users.noreply.github.com>
2022-07-03 23:01:07 +0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+$('.mobile div').on('click', () => {
+  $('.mobile div').toggleClass('active');
+  $('.mobile nav').toggleClass('open');
+  $('.mobile nav ul').toggleClass('show');
+});
+for (let a = 1; a <= $(".mobile ul li").length; a++) {
+  $(".mobile ul li:nth-child(" + a + ")").css("animation-delay", "." + (a + 1) + "s");
+  };
+  
+  
+$('.slider__box').slick({
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    fade: true,
+    cssEase: 'ease'
+
+});
